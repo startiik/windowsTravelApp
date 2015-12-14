9cd14718e5227a08b3f35c0432d0d3ec7a032291
Glenn Van Mele <glennvanmele@gmail.com>
2015-12-14 15:27:33 +0100

    no message

--- PackingList/App.xaml.cs
+++ PackingList/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.MobileServices;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -26,6 +27,10 @@ namespace PackingList
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
+        /// 
+        public static MobileServiceClient MobileService = new MobileServiceClient(
+         "https://packinglist.azure-mobile.net/",
+          "vfJliPfYTAjIaEWOVunWjURhoCOrJj48");
         public App()
         {
             this.InitializeComponent();
