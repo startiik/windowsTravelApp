538e853815299ac77e737adb1b96096719ca974e
Glenn Van Mele <glennvanmele@gmail.com>
2015-12-16 14:52:49 +0100

    Update Service

--- TravelBBNService/DataObjects/Reis.cs
+++ TravelBBNService/DataObjects/Reis.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 
 namespace TravelBBNService.DataObjects {
     public class Reis : EntityData {
+        public string UserID { get; set; }
         public string Name { get; set; }
         public string Location { get; set; }
         public DateTime DepartureDate { get; set; }
